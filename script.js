7cf58de61dd0ca63eeb58b8a8c40ca2a914e1a98
qwertyabu <monkey.famil4@gmail.com>
2025-01-29 16:48:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+ 
+let any = ('до')
+console.log(any)
+
+ for(let i = 2; i < 5; i+=2){
+ console.log(i);     
+}
+for(let i = 9; i < 11; i++){
+ console.log(i);     
+}
+for(let i = 45; i < 46; i++){
+    console.log(i);     
+   }
+
+   for(let i = 67; i < 68; i++){
+    console.log(i);     
+   }
+
+   
+   for(let i = 8; i < 9; i++){
+    console.log(i);     
+   }
+
+   
+   for(let i = 90; i < 91; i++){
+    console.log(i);     
+   }
+
+
+
+
+
+
+
+
+
+
+let some = ('после')
+console.log(some)
+
+ for(let i = 2; i < 5; i+=2){
+ console.log(i);     
+}
+for(let i = 10; i < 11; i++){
+ console.log(i);     
+}
+for(let i = 8; i < 9; i++){
+    console.log(i);     
+   }
+
+   for(let i = 90; i < 91; i++){
+    console.log(i);     
+   }
+
